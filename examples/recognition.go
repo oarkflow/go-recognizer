8f0e75496c62160c4ce16a86a30123db4b458b9b
Oarkflow <info@oarkflow.com>
2023-10-11 15:46:46 +0545

    Initial Commit

---
+++ examples/recognition.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/leandroveronezi/go-recognizer"
+)
+
+func addFile(rec *recognizer.Recognizer, Path, Id string) {
+
+	err := rec.AddImageToDataset(Path, Id)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
+func main() {
+	const fotosDir = "fotos"
+	const dataDir = "models"
+
+	rec := recognizer.Recognizer{}
+	err := rec.Init(dataDir)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rec.Tolerance = 0.4
+	rec.UseGray = true
+	rec.UseCNN = false
+	defer rec.Close()
+
+	addFile(&rec, filepath.Join(fotosDir, "amy.jpg"), "Amy")
+	addFile(&rec, filepath.Join(fotosDir, "bernadette.jpg"), "Bernadette")
+	addFile(&rec, filepath.Join(fotosDir, "howard.jpg"), "Howard")
+	addFile(&rec, filepath.Join(fotosDir, "penny.jpg"), "Penny")
+	addFile(&rec, filepath.Join(fotosDir, "raj.jpg"), "Raj")
+	addFile(&rec, filepath.Join(fotosDir, "sheldon.jpg"), "Sheldon")
+	addFile(&rec, filepath.Join(fotosDir, "leonard.jpg"), "Leonard")
+
+	rec.SetSamples()
+
+	faces, err := rec.ClassifyMultiples(filepath.Join(fotosDir, "elenco3.jpg"))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	img, err := rec.DrawFaces(filepath.Join(fotosDir, "elenco3.jpg"), faces)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rec.SaveImage("faces.jpg", img)
+
+}
